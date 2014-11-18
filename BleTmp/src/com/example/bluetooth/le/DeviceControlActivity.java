56b8f658437265e4c609b14788e1e6f3de16ef2c
sunzhanpeng <sunzhanpe@gmail.com>
2014-11-18 10:40:33 +0800

    start debug

--- BleTmp/src/com/example/bluetooth/le/DeviceControlActivity.java
+++ BleTmp/src/com/example/bluetooth/le/DeviceControlActivity.java
@@ -336,8 +336,7 @@ public class DeviceControlActivity extends Activity {
 			}
 				*/
 //chuck 说多传感器要 分时读取，不太明白啥意思
-//试试密码保存了么
-				//好用么
+
 			if(accCharacteristic[0] != null)
 			{
 				mBluetoothLeService.readCharacteristic(accCharacteristic[0]);
